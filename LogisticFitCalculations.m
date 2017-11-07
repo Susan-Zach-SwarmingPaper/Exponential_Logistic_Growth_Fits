119b20b85ca3142de3e74b52f67b5251db9620e8
selegget <33294545+selegget@users.noreply.github.com>
2017-11-06 20:10:31 -0500

    Add files via upload

---
+++ LogisticFitCalculations.m
@@ -0,0 +1,417 @@
+function LogisticFitCalculations
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%This Code Created 2 June 2017 by Zachary Neronha for the purpose of
+%calculating best fit parameters for cell culture modeled as logistic
+%growth
+%23 July 2017 Run
+%Version 2.0 written 11:54 3 August 2017 . Updated to be faster and more user
+%friendly
+%Version 2.1 written 16:08 3 August 2017 Loads scaled files and calculates 
+%number of cells on its own based on position
+%Version 3.0 written 13:51 5 August 2017: implements exponential fit in
+%same code, further commenting and bug fixes: Functional
+
+clearvars                                                            
+close all
+clc
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%set desired files to retrieve
+prompt = 'Which well(s) would you like to evaluate? Enter in matrix if more than 1.\n';
+desiredwells = input(prompt);
+%also apply exponential?
+prompt = 'The code will implement a logistic growth fit. Would you also like to implement\n an exponential fit for controls? 1==yes 0 == no\n';
+exp = input(prompt);
+
+% if exponential analysis is to be completed
+if exp ==1
+   prompt = 'Would you like to save the exponential plot? (1=yes 0=no)\n';
+   expimagesave = input(prompt);
+   if expimagesave == 1 %determine file name and path and store them
+       prompt = 'Enter name of file to be saved without extension and with single quotes\n';
+       expimagename = input(prompt);
+       expimagepath = uigetdir('Z:\ENG_BBCancer_Shared','Where would you like to store the image file?');
+       expimagename = strcat(expimagepath,'\',expimagename);    
+   end
+end
+
+%see if the user wants a spreadsheet of results
+prompt = 'Would you like to write the data to an organized spreadsheet? (0=no 1=yes)\n';
+excelwrite = input(prompt);
+%if yes define the file name and paths
+if excelwrite == 1
+    prompt = 'What would you like to call the output file (omit extension and enclose in single quotes)\n';
+    excelfilename = input(prompt);
+    excelpath = uigetdir('Z:\ENG_BBCancer_Shared','Where would you like to store the excel file?');
+end
+
+%do a similar thing for logistic growth images
+prompt = 'Would you like to save the logistic growth image? (1=yes 0=no)\n';
+savestatus = input(prompt);
+if savestatus == 1
+   prompt = 'Enter name of file to be saved without extension and with single quotes\n';
+   imagename = input(prompt);
+   imagepath = uigetdir('Z:\ENG_BBCancer_Shared','Where would you like to store the logistic image file?');
+   imagename = strcat(imagepath,'\',imagename);
+end
+disp('ALL PARAMETERS NOW ENTERED PLEASE WAIT FOR CODE COMPLETION')
+
+tic
+%create an empty matrix we will later fill with the final values
+AggMat = zeros(numel(desiredwells),6);
+storecounter = 1; %create a counter for the purposes of indexing
+
+%define the color scheme
+cmap = cbrewer('qual','Set1',numel(desiredwells));
+colorcounter = 1; %create a color indexer
+
+%implement dimensional checks, repairs, and error alerts
+if size(desiredwells,1) == 1
+else
+    desiredwells = desiredwells';
+    if size(desiredwells,1) == 1
+    else
+        error('PROGRAM CRASH: THE DESIRED WELLS MUST BE ENTERED AS A 1 by n MATRIX');
+    end
+end
+
+%loop over the desired files
+for currentwell = desiredwells
+    filename = strcat('Z:\ENG_BBCancer_Shared\group\0Zach\Cluster data\EGF (E6) Data',...
+        '\egf(E6)newwell',num2str(currentwell),'.mat'); %set the new file name
+    load(filename); %load that file
+    
+    %setup overall matrix from the position data
+    overall = NaN(size(storeX,2),2); %#ok<NODEF>
+    overall(:,2) = [1:size(storeX,2)]'; %#ok<NBRAK>
+    for aa = 1:size(storeX,2)
+        %figure out now many actual cells there are in a timepoint
+        m = isnan(storeX(:,aa)) == 0;
+        overall(aa,1) = sum(m);
+    end
+    
+    %assign variable names from imported files
+    pop = overall(:,1);
+    %pop = pop./(pop(1));
+    timept = overall(:,2);
+    %Assign appropriate time points in hours
+    timehr = timept./4;
+
+    %reference below function to get best logistic growth fit
+    [cf, G] = L4P(timehr,pop);
+    z = coeffvalues(cf); %extract coefficient values
+    %record relevant values from best fit curve
+    Init = z(1);
+    HillSlope = z(2);
+    Inflex = z(3);
+    CarCap = z(4);
+    %extract the correlation coefficient from the G Structure
+    CorCoeff = extractfield(G,'rsquare');
+
+    %Store the values in AggMat 
+    Vals = [currentwell, Init, HillSlope, Inflex, CarCap, CorCoeff];
+    AggMat(storecounter,:) = Vals;
+    storecounter = storecounter + 1;
+    
+    %plot the data
+    h = plot(cf,'r');
+    %turn repetitive legend entries off
+    set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+    hold on %enable dual plotting 
+    plot(timehr,pop,'linewidth',2,'Color',cmap(colorcounter,:))
+    colorcounter = colorcounter + 1;
+
+end
+    %tidy up the plot
+    ylabel('Number of Cells')
+    xlabel('Time in Hours')
+    title('Logistic Growth Plot')
+    
+    %construct a variable custom array for the legend
+    legendname = cell(1,numel(desiredwells));
+    legendindex = 1; %create an index
+    for aa = desiredwells %loop through the wells, storing each in the labeling cell
+       legendname{1,legendindex} = strcat('Well',num2str(aa));
+        legendindex = legendindex + 1;
+    end
+    
+    legend(legendname,'location','nw')
+    
+    %write to an excel file
+    if excelwrite == 1
+        disp('PLOTTING NOW COMPLETE--WRITING EXCEL FILE');
+    % Turn the matrix into a nicely labeled table
+    Final = array2table(AggMat,'VariableNames',{'Well','InitialDensity', ...
+         'HillSlope','InflexionPoint','CarryingCapacity','CorrelationCoefficient'});
+    %Write to an Excel File
+    ExcelFile = strcat(excelpath,'\',excelfilename,'.xlsx');
+    writetable(Final,ExcelFile)
+    end
+    %...and an image
+    if savestatus == 1
+    disp('EXCEL FILE IS WRITTEN--SAVING HIGH QUALITY IMAGE FILES (THIS MAY TAKE SOME TIME)');
+    print(gcf,imagename,'-depsc','-r2000')
+    print(gcf,imagename,'-dpng','-r2000')
+    end
+    %alert the user of progresss
+    disp('LOGISTIC GROWTH FIT IS NOW COMPLETED AND SAVED!');
+    
+    %IF the user wants to investigate standard exponential fits
+    if exp == 1
+        %open a new figure and reset the color counter
+        figure; 
+        colorcounter = 1;
+
+        for currentwell = desiredwells %loop through the specified wells
+            filename = strcat('Z:\ENG_BBCancer_Shared\group\0Zach\Cluster data\EGF (E6) Data',...
+                '\egf(E6)newwell',num2str(currentwell),'.mat'); %set the new file name
+            load(filename); %load that file
+
+            %setup overall matrix from the position data
+            overall = NaN(size(storeX,2),2);
+            overall(:,2) = [1:size(storeX,2)]'; %#ok<NBRAK>
+            for aa = 1:size(storeX,2)
+                %figure out now many actual cells there are in a timepoint
+                m = isnan(storeX(:,aa)) == 0;
+                overall(aa,1) = sum(m);
+            end
+    
+            pop = overall(:,1);
+
+            time = overall(:,2)./4;
+            [z, r] = fit(time,pop,'exp1');
+            r = extractfield(r,'rsquare');
+            disp(strcat('The Correlation Coefficient is',{' '},num2str(r),' For well ',num2str(currentwell)));
+            vals = coeffvalues(z); %extract coefficient values
+            disp(strcat('The Doubling time for well',{' '},num2str(currentwell),{' is '},num2str(log(2)/vals(2)),' hours'));
+
+            h = plot(z);
+            set(get(get(h,'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
+            hold on
+            plot(time,pop,'color',cmap(colorcounter,:),'linewidth',2)
+            colorcounter = colorcounter + 1;
+            hold on
+        end
+        title('Exponential Growth Plot')
+        xlabel('Time (hours)')
+        ylabel('Number of Cells')
+
+        %construct a variable custom array for the legend
+        legendname = cell(1,numel(desiredwells));
+        legendindex = 1; %create an index
+        for aa = desiredwells %loop through the wells, storing each in the labeling cell
+           legendname{1,legendindex} = strcat('Well',num2str(aa));
+            legendindex = legendindex + 1;
+        end
+    
+        legend(legendname,'location','nw')
+        
+        if expimagesave == 1
+        disp('SAVING HIGH QUALITY Exponential IMAGE FILES (THIS MAY TAKE SOME TIME)');
+        print(gcf,expimagename,'-depsc','-r2000')
+        print(gcf,expimagename,'-dpng','-r2000')
+        end
+        disp('The Exponential portion of the analysis is now complete')  
+    end
+    disp('CODE ANALYTICS NOW COMPLETE');
+    toc
+   load handel.mat
+   sound(y);
+    
+end
+
+function [cf, G]=L4P(x,y,varargin)
+%L4P Four Parameters logistic regression
+% The Four Parameters Logistic Regression or 4PL nonlinear regression model
+% is commonly used for curve-fitting analysis in bioassays or immunoassays
+% such as ELISAs or dose-response curves. 
+% It is characterized by it’s classic “S�? or sigmoidal shape that fits the
+% bottom and top plateaus of the curve, the EC50, and the slope factor
+% (Hill slope). This curve is symmetrical around its inflection point. 
+%
+% The 4PL equation is:
+% F(x) = D+(A-D)/(1+(x/C)^B)
+% where:
+% A = Minimum asymptote. In a bioassay where you have a standard curve,
+% this can be thought of as the response value at 0 standard concentration.
+%
+% B = Hill's slope. The Hill's slope refers to the steepness of the curve.
+% It could either be positive or negative.
+%
+% C = Inflection point. The inflection point is defined as the point on the
+% curve where the curvature changes direction or signs. C is the
+% concentration of analyte where y=(D-A)/2.
+%
+% D = Maximum asymptote. In an bioassay where you have a standard curve,
+% this can be thought of as the response value for infinite standard
+% concentration. 
+% 
+%
+% Syntax: [cf G]=L4P(x,y,st,L,U)
+% 
+% Inputs: 
+%           X and Y (mandatory) - data points.
+%           X is a Nx1 column vector and Y must have the same rows number
+%           of X. If Y is a NxM matrix (N points and M replicates), L5P
+%           will generate a  column vector computing means for each row.
+%           The standard deviations of the rows will be used as weights of
+%           regression.
+%
+%           st = starting points. This is a 1x4 vector of starting points
+%           that have to be used to start the process of not linear
+%           fitting. If this vector is not provided, L4P will set the
+%           starting points on the basis of x and y data.
+%
+%           L = Lower bounds of parameters. This is a 1x4 vector of lower
+%           bounds of the 4 parameters. If this vector is not provided, L4P
+%           will set it on the basis of x and y data.
+%
+%           U = Upper bounds of parameters. This is a 1x4 vector of upper
+%           bounds of the 4 parameters. If this vector is not provided, L4P
+%           will set it on the basis of x and y data.
+%
+% Outputs:
+%           cf = the FIT object
+%           G = goodness-of-fit measures, for the given inputs, in the
+%           structure G. G includes the fields: 
+%           -- SSE         sum of squares due to error
+%           -- R2          coefficient of determination or R^2
+%           -- adjustedR2  degree of freedom adjusted R^2
+%           -- stdError    fit standard error or root mean square error
+% 
+% Example:
+%
+% x=[0 4.5 10.6 19.7 40 84 210]; y=[0.0089 0.0419 0.0873 0.2599 0.7074 1.528 2.7739];
+%
+% Calling on MatLab the function: [cf G]=L4P(x,y)
+% 
+%           Answer is:
+% 
+% cf = 
+% 
+%      General model:
+%      cf(x) = D+(A-D)/(1+(x/C)^B)
+%      Coefficients (with 95% confidence bounds):
+%        A =    0.001002  (-0.04594, 0.04794)
+%        B =       1.515  (1.293, 1.738)
+%        C =         108  (86.58, 129.4)
+%        D =       3.784  (3.302, 4.266)
+% 
+% G = 
+% 
+%            sse: 0.0012
+%        rsquare: 0.9998
+%            dfe: 3
+%     adjrsquare: 0.9996
+%           rmse: 0.0200
+%
+% hold on; plot(x,y,'ro'); plot(cf,'r'); hold off
+% this will plot the curve.
+%
+%           Created by Giuseppe Cardillo
+%           giuseppe.cardillo-edta@poste.it
+%
+% See also L4Pinv, L5P, L5Pinv, L3P, L3Pinv
+%
+% To cite this file, this would be an appropriate format:
+% Cardillo G. (2012) Four parameters logistic regression - There and back again
+
+
+%--------------------Input errors handling section-------------------------
+if nargin < 2
+    error('Almost X and Y vectors are required')
+end
+
+x=x(:); 
+if ~isvector(x)
+    error('X must be a column vector')
+end
+
+%if y is a matrix, compute means and standard deviations
+we=zeros(size(x));
+if ~isvector(y) 
+    we=std(y,0,2);
+    y=mean(y,2);
+end
+y=y(:);
+
+if size(x)~=size(y)
+    error('X and Y must have the same raws number')
+end
+
+ok_ = isfinite(x) & isfinite(y);
+if ~all( ok_ )
+    warning('L4P:IgnoringNansAndInfs','Ignoring NaNs and Infs in data.');
+end
+
+%To compute 4 parameters you almost need 4 points...
+if length(x(ok_))<4 
+        warning('L4P:NotEnoughData','Not enough Data points')
+end
+
+%set or check optional input data
+args=cell(varargin);
+nu=numel(args);
+default.values = {[],[],[]};
+default.values(1:nu) = args;
+[st_, L, U] = deal(default.values{:});
+
+%set the starting points:
+% A is the lower asymptote so guess it with min(y)
+% B is the Hill's slope so guess it with the slope of the line between first and last point.
+% C is the inflection point (the concentration of analyte where you have
+% half of the max response) so guess it finding the concentration whose
+% response is nearest to the mid response.
+% D is the upper asymptote so guess it with max(y)
+
+slope=(y(end)-y(1))/(x(end)-x(1));
+if isempty(st_)
+    [~,Idx]=min(abs((y-((max(y)-min(y))/2))));
+    st_=[min(y) sign(slope) x(Idx) max(y)];
+else
+    st_=st_(:)';
+    if length(st_)~=4
+        error('Starting points array must be a 1x4 array')
+    end
+end
+
+%set the bounds. Of course all lower bounds are 0 and all upper bound are
+%Inf. Anyway, if the slope is negative the lower bound of B is -Inf and the
+%upper bound is 0. 
+
+if isempty(L)
+    L=zeros(1,4);
+    if slope<0
+        L(2)=-Inf;
+    end
+else
+    L=L(:)';
+    if length(L)~=4
+        error('Lower bounds array must be a 1x4 array')
+    end
+end
+
+if isempty(U)
+    U=Inf(1,4);
+    if slope<0
+        U(2)=0;
+    end
+else
+    U=U(:)';
+    if length(L)~=4
+        error('Upper bounds array must be a 1x4 array')
+    end
+end
+clear args default nu slope Idx
+
+%-----------------------------Fit the data---------------------------------
+fo_ = fitoptions('method','NonlinearLeastSquares','Lower',L,'Upper',U);
+set(fo_,'Startpoint',st_);
+if all(we) % if y was a matrix use std as weights for fitting
+    set(fo_,'Weights',we);
+end
+ft_ = fittype('D+(A-D)/(1+(x/C)^B)',...
+     'dependent',{'y'},'independent',{'x'},...
+     'coefficients',{'A', 'B', 'C', 'D'});
+ [cf, G] = fit(x(ok_),y(ok_),ft_,fo_);
+end
